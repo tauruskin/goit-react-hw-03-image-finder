b72aac6ac9813eb35893d74c3322a06f4223d5a4
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-09-10 12:58:05 +0300

    создает галерею

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ImageGalleryItems from '../ImageGalleryItem/ImageGalleryItem';
+import Button from '../App';
+import './ImageGallery.css';
+
+const ImageGallery = ({ data, loadMore, largeImg }) => {
+  console.log(data);
+  return (
+    <>
+      <ul className="ImageGallery">
+        {data.map(item => (
+          <ImageGalleryItems
+            key={item.id}
+            imageInfo={item}
+            largeImg={largeImg}
+          />
+        ))}
+      </ul>
+      <div className="load_more">
+        {data.length !== 0 && <Button loadMore={loadMore} />}
+      </div>
+    </>
+  );
+};
+export default ImageGallery;
