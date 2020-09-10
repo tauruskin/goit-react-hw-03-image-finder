e8f649ed961b021c43b059819ca3e167999eb68a
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-09-10 12:57:48 +0300

    закрывает модальное окно

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Searchbar from '../Searchbar/Searchbar';
-import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import ImageGallery from '../';
 import Modal from '../Modal/Modal';
 import Loader from '../Loader/Loader';
 
@@ -14,6 +14,73 @@ class App extends Component {
     loading: false,
     largeImageURL: null,
   };
+  async componentDidUpdate(prevState) {
+    if (
+      prevState.search !== this.state.search ||
+      prevState.page !== this.state.page
+    ) {
+      const { search, page, per_page } = this.state;
+      const baseUrl = 'https://pixabay.com/api/?q';
+      await axios
+        .get(
+          `${baseUrl}=${search}&page=${page}&key=${process.env.REACT_APP_KEY}&per_page=${per_page}`,
+        )
+        .then(data => {
+          this.setState(prev => ({
+            articles: [...prev.articles, ...data.data.hits],
+            loading: false,
+          }));
+          window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
+        });
+    }
+  }
+  setLargeImage = url => {
+    console.dir(url.target.dataset.large);
+    this.setState({
+      largeImageURL: url.target.dataset.large,
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      page: 1,
+      articles: [],
+      search: e.target[1].value,
+      loading: true,
+    });
+  };
+  loadMore = async () => {
+    const { page } = this.state;
+    await this.setState({
+      page: page + 1,
+    });
+    console.log(this.state.page);
+    console.log(this.state.per_page);
+  };
+
+  closeModal = () => {
+    this.setState({ largeImageURL: null });
+  };
+  render() {
+    const { articles } = this.state;
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.onSubmit} />
+        {this.state.loading ? (
+          <Loader />
+        ) : (
+          <ImageGallery
+            data={articles}
+            loadMore={this.loadMore}
+            largeImg={this.setLargeImage}
+          />
+        )}
+        {this.state.largeImageURL && (
+          <Modal info={this.state.largeImageURL} closeModal={this.closeModal} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
